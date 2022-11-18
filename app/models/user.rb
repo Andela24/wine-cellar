0bd94ead6b3c04df11d4e7ff8f956ead011d4091
Andela Stevanovic <andjela.s166@gmail.com>
2022-11-17 19:07:18 -0500

    set relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    has_secure_password
+
+    validates :username, presence: true, uniqueness: true
+    validates :password, presence: true
+
+    has_many :bottles
+    has_many :wineries, -> { distinct }, through: :bottles
 end
--- app/models/winery.rb
+++ app/models/winery.rb
@@ -1,2 +1,6 @@
 class Winery < ApplicationRecord
+    has_many :bottles
+    has_many :users, through: :bottles
+
+    # validates :name, presence: true
 end
